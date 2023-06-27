3da9930e7ce20582d987504d2ae21afe25940d63
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-06-27 22:37:17 +0900

    Create ch2.py

---
+++ Python Practice Codes/02-ch2/ch2.py
@@ -0,0 +1,23 @@
+# Data Type: Number
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+
+# Equation with print
+print(5+3)
+print(2*8)
+print(6/3)
+print(3*(3+1))
+
+# Data Type: Character
+print('balloon')
+print('butterfly')
+print('abcdefg')
+print("10")
+print("python"*3)
+
+# Data Type: Boolean
+print(5>10)
+print(5<10)
+print(not(5>10))
