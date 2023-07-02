b71e32160c3c1e9aa4b7f5cba6f5f542bfa7ab9d
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-07-02 23:28:29 +0900

    Create 01-simple_operators.py

---
+++ Python Practice Codes/05-operators/01-simple_operators.py
@@ -0,0 +1,25 @@
+print(1 + 1)
+print(3 - 2)
+print(5 * 2)
+print(6 / 3)
+
+print(2 ** 3)
+print(10 % 3)
+print(10 // 3)
+
+print(10 > 3)
+print(4 >= 7)
+print(10 < 3)
+print(5 <= 5)
+
+print(3 == 3)
+print(4 == 2)
+print(3 + 4 == 7)
+print(1 != 3)
+
+print((3>0) and (3>5))
+print((3>0) or (3>5))
+print(not(1 != 3))
+
+print(2 + 3 * 4)
+print((2 + 3) * 4)
