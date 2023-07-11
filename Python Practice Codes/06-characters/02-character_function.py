a901fda3bbf09790d26184ed7f0917e7a9ad9556
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-07-11 21:42:35 +0900

    Create 02-character_function.py

---
+++ Python Practice Codes/06-characters/02-character_function.py
@@ -0,0 +1,16 @@
+python = "Python is interesting"
+
+# Convert All Characters into lowercase
+print(python.lower())
+
+# Convert All Characters into Uppercase
+print(python.upper())
+
+# Check index 0 is uppercase or not. If True return True otherwise, False
+print(python[0].isupper())
+
+# Check index 1, 2 are lowercase or not. If True return True otherwise, False
+print(python[1:3].islower())
+
+# Change Python to Java
+print(python.replace("Python", "Java"))
