8076883dd8024d0d0350f2caabec3344f480b23e
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-07-11 22:26:03 +0900

    Update and rename 01-line_shift.py to 01-escape_characters.py

---
+++ Python Practice Codes/07-escape_character/01-escape_characters.py
@@ -0,0 +1,22 @@
+# Line Shifting
+print("Hello!\nI am Doyun GU :)")
+
+# '\' 
+print("I am 'Doyun GU' and 21 years old")
+print('I am "Doyun GU" and 21 years old')
+
+print("I am \"Doyun GU\" and 21 years old")
+print('I am \'Doyun GU\' and 21 years old')
+
+# '\\' printout 'File Directory'
+print("C:\\Users\\Doyun GU\\Desktop\\PythonWorkSpace")
+
+# 'r' printout whatever it contains
+print(r"C:\Users\Doyun GU\Desktop\PythonWorkSpace")
+
+# '\b' works as 'Back Space'. Delete One Space
+print("Redd\bApple")
+
+# '\t' works as 'Tab Space'.
+# Tab Spacing Setting (VS Code): File - Preference - Settings - Editting: Tab Size
+print("Red\tApple")
