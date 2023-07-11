a2ba47e2252cf99087fd6177787ec95e70a4b664
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-07-11 21:28:13 +0900

    Create 02-simple_functions.py

---
+++ Python Practice Codes/05-operators/02-simple_functions.py
@@ -0,0 +1,55 @@
+# asb(x): Return Absolute Value of x
+print(abs(-5))
+
+# pow(x, y): Return x power of y  
+print(pow(4,2))
+
+# max(x,y): Return maximum value which is y
+print(max(5, 12))
+
+# round(x, d): Return rounded value of x up to d
+print(round(2.678, 2))
+
+# Import All Math Functions
+from math import *
+from random import *
+
+result = floor(4.99)
+print(result)
+
+result = ceil(3.14)
+print(result)
+
+result = sqrt(16)
+print(result)
+
+# Return a random value between 0 and 1
+print(random())
+print(random())
+print(random())
+
+# a random value between 0 and 10
+print(random()*10)
+
+# a random value between 0 to 10 in integer data type
+print(int(random() * 10))
+
+# a random value between 1 to 11 in integer data type
+print(int(random()*10)+1)
+
+# randrange (beginning number, ending number): ending number is not included in the random number
+# randint (beginning number, ending number): ending number is included in the random number
+
+# 1 to 46
+print(randrange(1, 46))
+
+# 1 to 45
+print(randint(1, 45))
+
+print(randint(1, 45))
+print(randint(1, 45))
+print(randint(1, 45))
+print(randint(1, 45))
+print(randint(1, 45))
+print(randint(1, 45))
+print(randint(1, 45))
