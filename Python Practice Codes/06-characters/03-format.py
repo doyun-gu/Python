1217e54833ba9d31a917009ee19eae25ab95f1e0
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-07-11 22:09:26 +0900

    Create 03-format.py

---
+++ Python Practice Codes/06-characters/03-format.py
@@ -0,0 +1,7 @@
+print("I am {0} years old and love {1}" .format("21", "Python"))
+print("I love {1} and am {0} years old" .format("21", "Python"))
+
+# f-string converts all the variables into characters and this is only available from 3.6 version
+age = 21
+color = "gold"
+print(f"I am {age} years old, and like {color}")
