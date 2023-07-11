b6cb2de85f8b3e78c0140b8e68d10ecf9580b1dd
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-07-11 21:37:30 +0900

    Update 01-slicing.py

--- Python Practice Codes/06-characters/01-slicing.py
+++ Python Practice Codes/06-characters/01-slicing.py
@@ -1,1 +1,13 @@
+# All Koreans have their own ID number and 7th place indicates gender; 1 - Male (~1,999), 2- Female(~1,999) 3 - Male(~2,000), 4 - Female(~2,000)
 
+KOR_ID='990229-1234567'
+print("Gender: " + KOR_ID[7])
+
+print("Year:" + KOR_ID[0:2])
+print("Moth:" + KOR_ID[2:4])
+print("Date:" + KOR_ID[4:6])
+
+print("Birth Year, Month, Date:" + KOR_ID[:6])
+print("Second Part of ID:" + KOR_ID[7:])
+
+print("Second Part of ID:" + KOR_ID[-7:])
