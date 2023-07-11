208efb7c12a8a962727822f7637e9a345088de95
Doyun GU <128914278+doyun-gu@users.noreply.github.com>
2023-07-11 21:53:48 +0900

    Update 02-character_function.py

--- Python Practice Codes/06-characters/02-character_function.py
+++ Python Practice Codes/06-characters/02-character_function.py
@@ -14,3 +14,35 @@ print(python[1:3].islower())
 
 # Change Python to Java
 print(python.replace("Python", "Java"))
+
+# find(targeting Character, Beginning Character, Ending Character)
+# : if there's no targetting value in the given characters, it returns -1 and continue
+
+# index(targeting character, Beginning Character, Ending Character)
+# : if there;s no targetting value in the given characters, it returns ValueError:~ and exit
+
+# Return the index number where it found 'n' for the first time
+find = python.find("n")
+print(find)
+# Return where it found 'n' after index 6 (find: index 5)
+find = python.find("n", find+1)
+print(find)
+# Return '-1' since there's no 'Java'
+find = python.find("Java")
+print(find)
+
+# Return the index number where it found 'n' for the first time
+index = python.index("n")
+print(index)
+
+# Return where it found 'n' after index 6 (index: index 5)
+index = python.index("n", index + 1)
+print(index)
+
+# From second index and right before the 6th index(5) Return index number where it found 'n' 
+index = python.index("n", 2, 6)
+print(index)
+
+# Return ValueError:~ and exit the code since  there's no 'Java' in the given sentence
+index = python.index("Java")
+print(index)
